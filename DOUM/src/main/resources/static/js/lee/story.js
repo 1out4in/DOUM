4a52fbda0f45e7bf65ffcead059b2930d33f12e7
stella9921 <stella9921@naver.com>
2024-07-31 12:43:51 +0900

    0731 이현수 댓글삭제까지완

--- DOUM/src/main/resources/static/js/lee/story.js
+++ DOUM/src/main/resources/static/js/lee/story.js
@@ -45,39 +45,29 @@ $(document).ready(function () {
 // 댓글 목록 조회 함수
 function getComments(storyId) {
     $.ajax({
-        method : 'get',
-        url : '/comments/' + storyId,
-        success : function(data) {
-            // let commentListArea = $('.comment-list')
-            let commentListArea = $('.comment-form')
-
+        method: 'get',
+        url: '/comments/' + storyId,
+        success: function(data) {
+            let commentListArea = $('.comment-list');
 
             // 댓글이 작성될 해당 섹션 비우기.
             commentListArea.empty();
 
             // 댓글 없을 때 표시할 html
-            if(data.length === 0){
-                commentListArea.append(
-                    `<div class="alert alert-info">
-                         첫번째 댓글을 남겨주세요!
-                    </div>`
-                );
-            }
+            // if (data.length === 0) {
+            //     commentListArea.append(
+            //         `<div class="alert alert-info">
+            //              첫번째 댓글을 남겨주세요!
+            //         </div>`
+            //     );
+            // }
 
             // 댓글 있을 때 목록을 뿌려줄 반복문.
             data.forEach(function(comment) {
-                // let commentDate = formatDate(comment.commentRegisterDate);
                 let buttons = '';
-                // let editStr = '';
-
-                // 작성일과 수정일을 비교해서 html 에 다른 모양으로 표시.
-                // if(comment.commentUpdateDate !== comment.commentRegisterDate){
-                //     commentDate = formatDate(comment.commentUpdateDate);
-                //     editStr = ' (수정)';
-                // }
 
                 // 현재 로그인된 계정과 댓글 작성자가 동일하다면 만들어줄 버튼
-                // if(loginId === comment.providerId){
+                // if (loginId === comment.providerId) {
                 //     buttons = `
                 //         <div class="comment-actions">
                 //             <button onclick="updateComment(${comment.commentId})" class="btn btn-primary">수정</button>
@@ -86,14 +76,12 @@ function getComments(storyId) {
                 //     `
                 // }
 
-
-                    buttons = `
-                        <div class="comment-form">
-<!--                            <button onclick="updateComment(${comment.userId})" class="btn btn-primary">수정</button>-->
-                            <button onclick="deleteComment(${comment.userId})" class="btn btn-danger">삭제</button>
-                        </div>
-                    `
-
+                buttons = `
+                    <div class="comment-actions">
+                        <!-- <button onclick="updateComment(${comment.userId})" class="btn btn-primary">수정</button> -->
+                        <button onclick="deleteComment(${comment.userId})" class="btn btn-danger">삭제</button>
+                    </div>
+                `;
 
                 // 종합적으로 뿌려줄 html
                 let commentElement = `
@@ -102,54 +90,52 @@ function getComments(storyId) {
                             <div class="comment-user">${comment.userId}</div>
 
                             <p class="comment-content">${comment.content}</p>
-                            
+
                             <div class="comment-options"> <i class="fas fa-ellipsis-v"></i></div>
                             <!-- 수정, 삭제 버튼 -->
                             ${buttons}
                         </div>
                     </div>
-                `
+                `;
                 // 해당 섹션에 추가
-                // 댓글의 갯 수 만큼 차례대로 추가될 것이다.
                 commentListArea.append(commentElement);
-            })
+            });
         },
-        error : function(data) {
+        error: function(data) {
             console.error(data, "불러오기 실패");
         }
-    })
+    });
 }
 
 // 댓글 추가
 function addStoryComment() {
     let storyId = $('input[name="storyId"]').val();
     let commentContent = $('#commentContent').val();
-    let userId = $('input[name="userId"]').val();
-
+    // let userId = $('input[name="userId"]').val();
 
     // textarea 비어 있으면 경고
-    if(!commentContent){
+    if (!commentContent) {
         alert('내용을 입력하세요!');
         return;
     }
 
     $.ajax({
-        method : 'post',
+        method: 'post',
         url: '/comments',
         contentType: 'application/json',
         data: JSON.stringify({
             storyId: storyId,
-            commentContent: commentContent,
-            providerId : userId
+            commentContent: commentContent
+            // providerId: userId
         }),
-        success : function(data) {
-            $('#commentContent').val('')
+        success: function(data) {
+            $('#commentContent').val('');
             getComments(storyId);
         },
-        error : function(data) {
+        error: function(data) {
             console.error(data);
         }
-    })
+    });
 }
 
 
@@ -180,48 +166,48 @@ function deleteComment(commentId){
 }
 
 // 댓글 수정 폼 생성 함수
-function createEditForm(commentId, currentContent){
-    return`
-        <div class="mb-3">
-            <textarea class="form-control comment-edit-content" rows="3">${currentContent}</textarea>
-        </div>
-        <button class="btn btn-primary" onClick="editComment(${commentId})">수정완료</button>
-        <button class="btn btn-secondary" onClick="cancelEdit()">취소</button>
-    `;
-
-}
+// function createEditForm(commentId, currentContent){
+//     return`
+//         <div class="mb-3">
+//             <textarea class="form-control comment-edit-content" rows="3">${currentContent}</textarea>
+//         </div>
+//         <button class="btn btn-primary" onClick="editComment(${commentId})">수정완료</button>
+//         <button class="btn btn-secondary" onClick="cancelEdit()">취소</button>
+//     `;
+//
+// }
 
 // 수정 삭제 버튼 중 수정을 눌렀을 때
-function updateComment(commentId) {
-    // 기존 댓글 내용을 가지고 와서, 수정 폼에 넣는다.
-    let comment = $(`#comment-${commentId}`);
-    let content = comment.find('.comment-text').text()
-    comment.find('.comment-body').html(createEditForm(commentId, content))
-}
+// function updateComment(commentId) {
+//     // 기존 댓글 내용을 가지고 와서, 수정 폼에 넣는다.
+//     let comment = $(`#comment-${commentId}`);
+//     let content = comment.find('.comment-text').text()
+//     comment.find('.comment-body').html(createEditForm(commentId, content))
+// }
 
 //수정 완료 버튼 눌렀을 때
-function editComment(commentId){
-    let comment = $(`#comment-${commentId}`);
-    //textarea는 속성이 두개
-    //데이터 뿌려줄 때는 text, 입려한 데이터를 가져올 대는 value
-    let updateContent = comment.find('.comment-edit-content').val()
-
-    $.ajax({
-        method : 'put',
-        url : '/comments/' + commentId,
-        contentType: 'application/json',
-        data: JSON.stringify({
-            commentContent: updateContent
-        }),
-        success : function(data) {
-            console.log('수정 성공')
-            getComments($('input[name="storyId"]').val());
-        },
-        error : function(data) {
-            console.error('수정 삭제')
-        }
-    })
-}
+// function editComment(commentId){
+//     let comment = $(`#comment-${commentId}`);
+//     //textarea는 속성이 두개
+//     //데이터 뿌려줄 때는 text, 입려한 데이터를 가져올 대는 value
+//     let updateContent = comment.find('.comment-edit-content').val()
+//
+//     $.ajax({
+//         method : 'put',
+//         url : '/comments/' + commentId,
+//         contentType: 'application/json',
+//         data: JSON.stringify({
+//             commentContent: updateContent
+//         }),
+//         success : function(data) {
+//             console.log('수정 성공')
+//             getComments($('input[name="storyId"]').val());
+//         },
+//         error : function(data) {
+//             console.error('수정 삭제')
+//         }
+//     })
+// }
 //취소 버튼 누른 경우
 function cancelEdit(){
     getComments($('input[name="storyId"]').val());
